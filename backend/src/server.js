68cb5ce67253c92e05b343a4c0b8f7ace0bc6470
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-11-11 16:02:05 +0530

    added notificaton and comments route

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,15 @@ import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 app.use(cors());    
 app.use(express.json());
 app.use(clerkMiddleware());
-
+app.use(arcjetMiddleware);
 
 
 app.get("/", (req, res) => {
@@ -21,6 +24,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // error handling middleware
 app.use((err, req, res, next) => {
