ee9d61f35f32a1a079375f2664fe3d9433831b9c
Sai Kumar Kindigeri <132796829+saikumarkindigeriDev@users.noreply.github.com>
2025-11-11 15:25:31 +0530

    completed user routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,33 @@
-import express from 'express';
-import { ENV } from './config/env.js';
-import { connectDB } from './config/db.js';
-
-const app=express();
-
-connectDB();
+import express from "express";
+import cors from "cors";
+import {clerkMiddleware} from "@clerk/express";
 
 
-app.get("/", (req,res)=>{
-    res.send("Welcome to X Twitter Clone Backend");
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+const app = express();
+app.use(cors());    
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/users",userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to X Twitter Clone Backend");
 });
 
 
-app.listen(ENV.PORT , ()=>{
-    console.log('Server is running on port: ', ENV.PORT);
-}) 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (err) { 
+    console.error("Error starting the server", err);
+    process.exit(1);
+  }
+};
+
+startServer();
